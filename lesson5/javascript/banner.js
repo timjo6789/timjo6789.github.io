6b0dc903df076afdfc66dfda23e9350d13e485eb
Timothy Johnson <tgj2u2@gmail.com>
2020-10-30 12:20:38 -0600

    updating lesson5 to comply

--- lesson5/javascript/banner.js
+++ lesson5/javascript/banner.js
@@ -1,4 +1,4 @@
-if (new Date().getDay() == 6){
+if (new Date().getDay() == 5){
   let banner = document.querySelector('div#banner');
   banner.textContent = "Saturday = Preston Pancakes in the Park! 9:00 a.m. Saturday at the city park pavilion.";;
   banner.removeAttribute('class', 'hidden');  
