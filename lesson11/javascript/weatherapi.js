722f34b699b149808e9db7c0375fec052c9a42ff
Timothy Johnson <tgj2u2@gmail.com>
2020-11-24 12:39:59 -0700

    setting up lesson11

--- lesson11/javascript/weatherapi.js
+++ lesson11/javascript/weatherapi.js
@@ -1,14 +1,33 @@
 const APPID = "25ade7342d9ba6641eee96d1d854f3b1";
-const PRESTION_ID = 5604473;
 
-fetch(`https://api.openweathermap.org/data/2.5/weather?id=${PRESTION_ID}&appid=${APPID}&units=imperial`)
-  .then((response) => response.json())
-  .then((json_data) => {
-    document.querySelector('span#temperature').textContent = json_data["main"]["temp"] + '°F';
+setText = (querySelector, text) => document.querySelector(querySelector).textContent = text;
 
-    let image = document.querySelector('img#icon');
-    image_source = `https://api.openweathermap.org/img/w/${json_data['weather'][0]['icon']}.png`;
-    image.setAttribute('src', image_source);
-    image.setAttribute('alt', json_data['weather'][0]['description']);
-    document.querySelector('span#image_source').textContent = image_source;
-  });
+function weather(json_data){
+  wind_speed = json_data['wind']['speed'];
+  temperature = json_data['main']['temp'];
+
+  setText('span#wind_speed', wind_speed);
+  setText('span#temperature', temperature);
+  setText('span#wind_chill', wind_chill_calculate(temperature, wind_speed));
+  setText('span#humidity', json_data['main']['humidity']);
+}
+
+function forecast(json_data){
+  weather_bottom = document.querySelectorAll(`span.weather-bottom`);
+  forcasts = json_data['list'].filter(each => each['dt_txt'].split(' ')[1] == '18:00:00');
+  for (let i = 0; i < 5; i++)
+    weather_bottom[i].innerHTML = `${api_image(forcasts[i])} ${forcasts[i]['main']['temp']}°F`;
+}
+
+function api_image(json_data){
+  json_data = json_data['weather'][0];
+  source = `https://api.openweathermap.org/img/w/${json_data['icon']}.png`;
+  return `<img src="${source}" alt="${json_data['description']}">`;
+}
+
+bind_fetch = (function_call, link) => fetch(link).then((response) => response.json()).then((json_data) => function_call(json_data));
+template = (type, id) => `https://api.openweathermap.org/data/2.5/${type}?id=${id}&appid=${APPID}&units=imperial`;
+
+// if using this file, provide TOWN_ID from another js file or from script tag prior to this
+bind_fetch(weather, template('weather', TOWN_ID));
+bind_fetch(forecast, template('forecast', TOWN_ID));
